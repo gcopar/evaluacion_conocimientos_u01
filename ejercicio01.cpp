d0511b160249de1c6860be9f87f17955a3050672
gcopar <gcopar@unjbg.edu.pe>
2024-06-24 04:14:28 -0500

    Actualiza ejercicio01: agrega repeticion para operaciones no validas

--- ejercicio01.cpp
+++ ejercicio01.cpp
@@ -5,32 +5,46 @@
 using namespace std;
 
 int main(){
-    int op1, op2, r;
+    int op1, op2, r, control;
     char op;
 
-    cout<<"Ingresa el primer operando: "; cin>>op1;
-    cout<<"Ingresa el segundo operando: "; cin>>op2;
-    cout<<"Ingresa la operacion: "; cin>>op;
-
-    switch (op)
+    do
     {
-    case '+':
-        r = op1 + op2;
-        break;
-    case '-':
-        r = op1 - op2;
-        break;
-    case '*':
-        r = op1 * op2;
-        break;
-    case '/':
-        r = op1 / op2;
-        break;
-    default:
-        cout<<"Ingrese un operador valido";
-        break;
-    }
+        cout<<"Ingresa el primer operando: "; cin>>op1;
+        cout<<"Ingresa el segundo operando: "; cin>>op2;
+        cout<<"Ingresa la operacion: "; cin>>op;
 
+        switch (op)
+        {
+        case '+':
+            r = op1 + op2;
+            control = 1;
+            break;
+        case '-':
+            r = op1 - op2;
+            control = 1;
+            break;
+        case '*':
+            r = op1 * op2;
+            control = 1;
+            break;
+        case '/':
+            if (op2!=0)
+            {
+                r = op1 / op2;
+                control = 1;
+            }else{
+                cout<<"Operacion matematica no definida, ingresa un valor diferente a 0 para el denominador."<<endl;
+                control = 0;
+            }
+            break;
+        default:
+            cout<<"Ingrese un operador valido";
+            control = 0;
+            break;
+        }   
+    } while (control != 1);
+    
     cout<<"El resultado de "<<op1<<" "<<op<<" "<<op2<<" es: "<<r<<endl;
     return 0;
 }
