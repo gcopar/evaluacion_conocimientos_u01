a334f2ba5642a28a1da912a9670fef350a0d11c9
gcopar <gcopar@unjbg.edu.pe>
2024-06-24 03:28:11 -0500

    Se agrega ejercicio03

---
+++ ejercicio03.cpp
@@ -0,0 +1,40 @@
+//Nombre: Gustavo Mario de la Cruz Copa Romero
+//Codigo: 2021-177006
+
+#include <iostream>
+using namespace std;
+
+float suma(int a);
+int factorial(int b);
+
+int main(){
+    int n;
+    float sum;
+    cout<<"Ingrese la cantidad de sumando: "; cin>>n;
+
+    sum = suma(n);
+    cout<<"La sumatoria es: "<<sum<<endl;
+    return 0;
+}
+
+float suma(int a){
+    float s;
+    s = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        s = s + float(factorial(i))/(2*i);
+    }
+    return s;
+}
+
+int factorial(int b){
+    int f;
+
+    f = 1;
+    for (int i = 2; i <= b; i++)
+    {
+        f = f * i;
+    }
+    return f;
+}
