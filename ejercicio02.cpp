4087c48d9b0fa12789ac9621d2e6501947579211
gcopar <gcopar@unjbg.edu.pe>
2024-06-24 04:37:09 -0500

    Actualiza ejercicio02: agrega repeticion del programa si los numeros son menor igual que 0

--- ejercicio02.cpp
+++ ejercicio02.cpp
@@ -5,29 +5,39 @@
 using namespace std;
 
 int main(){
-    int n1, n2, c, i;
+    int n1, n2, c, i, control;
 
-    cout<<"Ingrese el primer numero: "; cin>>n1;
-    cout<<"Ingrese el segundo numero: "; cin>>n2;
-
-    c = 0;
-    i = 1;
-
-    while (i<=n1 and i<=n2)
+    do
     {
-        if (n1 % i == 0 and n2 % i == 0)
+        cout<<"Ingrese el primer numero: "; cin>>n1;
+        cout<<"Ingrese el segundo numero: "; cin>>n2;
+
+        c = 0;
+        i = 1;
+        if (n1 > 0 and n2 > 0)
         {
-            c = c + 1;
+            while (i<=n1 and i<=n2)
+            {
+                if (n1 % i == 0 and n2 % i == 0)
+                {
+                    c = c + 1;
+                }
+                i = i + 1;
+            }
+
+            if (c > 1)
+            {
+                cout<<"No son PESI"<<endl;
+            }else{
+                cout<<"Son PESI"<<endl;
+            }
+            control = 1;
+        }else{
+            cout<<"Ingrese numeros iguales o mayores que 1."<<endl;
+            control = 0;
         }
-        i = i + 1;
-    }
-    
-    if (c > 1)
-    {
-        cout<<"No son PESI"<<endl;
-    }else{
-        cout<<"Son PESI"<<endl;
-    }
-    
+        
+    } while (control != 1);
+
     return 0;
 }
